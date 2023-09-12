667dcb0af0e83336a56932c0fda713bc2a64130c
beratisahin <beratisahin94@gmail.com>
2023-09-13 01:55:54 +0300

    v5

--- src/components/sections/Blog/Blog.js
+++ src/components/sections/Blog/Blog.js
@@ -19,11 +19,11 @@ const Blog = () => {
         <div className="section-content">
           <div className="row">
             <div className="col-lg-4 mb-3">
-              <div className="card rounded-0">
+              <div className="card rounded-0" >
                 <img src={blogImage1} className="card-img-top" alt="Blog 1" />
-                <div className="card-body">
+                <div className="card-body" >
                   <h5 className="card-title">Zayıflamada Krom Etkisi?</h5>
-                  <p className="card-text">
+                  <p className="card-text" style={{height:"300px"}}>
                     İştah artışı krom eksikliği yaşayanlarda ortaya çıkmaktadır.
                     Vücutta idrarla fazla krom atılınca kan şekeri yükselir ve
                     tatlıya karşı aşırı istek gözlenebilir. Bunun mekanizması
@@ -32,9 +32,7 @@ const Blog = () => {
                     sayesinde glukoza (şeker) parçalanır. Enerji üretmek için bu
                     glukoz vücudun belirli hücrelere: -Yağ hücreleri -Kas
                     hücreleri -Karaciğer hücrelerine girmek zorundadır. Ama bunu
-                    tek başına yapamaz. Bu iş için pankreastan salınan bir
-                    hormon olan insüline ihtiyaç duymaktadır. İnsülin sayesinde
-                    kanda dolaşan glukoz düzeyinde düşme gözlenir.
+                    tek başına yapamaz.
                   </p>
                   <a
                     href="https://www.instagram.com/eftalia.diamond.beauty/"
@@ -48,11 +46,11 @@ const Blog = () => {
               </div>
             </div>
             <div className="col-lg-4 mb-3">
-              <div className="card rounded-0">
+              <div className="card rounded-0" >
                 <img src={blogImage2} className="card-img-top" alt="Blog 2" />
                 <div className="card-body">
                   <h5 className="card-title">CİLT BAKIMI NEDEN LAZIM?</h5>
-                  <p className="card-text">
+                  <p className="card-text" style={{height:"300px"}}>
                     Cildimiz vücudumuzun en büyük organı olup birçok katmandan
                     oluşmuştur. Sinirler, salgı bezleri ,saç ve tırnakları
                     barındıran büyük bir sistemin parçasıdır. . En önemli görevi
@@ -60,10 +58,7 @@ const Blog = () => {
                     önemli soru: Bu bariyeri nasıl koruruz, cilt bakımı nasıl
                     yapılmalı? Cildimiz, vücudumuzun her yerinde aynı kalınlıkta
                     değildir. Yüzümüzde daha narinken avuç içleri ya da ayak
-                    tabanlarımızda daha kalındır. Esas olan cildin bariyerini
-                    sağlam tutmaktır. Bunu deriye hasar verebilecek ajanlardan
-                    kaçarak ve bol şekilde nemlendirerek sağlayabiliriz. Cilt
-                    sabah ve akşam uygun bir ürünü ile temizlenmelidir.
+                    tabanlarımızda daha kalındır. 
                   </p>
                   <a
                     href="https://www.instagram.com/eftalia.diamond.beauty/"
@@ -77,11 +72,11 @@ const Blog = () => {
               </div>
             </div>
             <div className="col-lg-4 mb-3">
-              <div className="card rounded-0">
+              <div className="card rounded-0" >
                 <img src={blogImage3} className="card-img-top" alt="Blog 3" />
                 <div className="card-body">
                   <h5 className="card-title">Göz Altı Morarması Neden Olur?</h5>
-                  <p className="card-text">
+                  <p className="card-text" style={{height:"300px"}}>
                     Aynaya baktığımızda bizleri karşılayan göz çevresindeki koyu
                     halkalar canımızı sıkar. Direkt olarak göz çevresi
                     morarmasının sebebini merak edilir. Bunun ile beraber göz
@@ -91,9 +86,7 @@ const Blog = () => {
                     çevresel sebeplere bağlı olarak göz çevresi morarması
                     oluşabilmektedir. Morarmanın başlıca sebepleri: • Göz
                     çevresindeki cildin narin bir yapıya sahip olması, • Mavi
-                    ışıkla uzun süre temas, • Doğum kontrol hapı, göz damlaları
-                    veya kanser ilaçları, • Dolaşım bozukluğu, • Uyku
-                    düzensizliği, • Genetik faktörler.
+                    ışıkla uzun süre temas
                   </p>
                   <a
                     href="https://www.instagram.com/eftalia.diamond.beauty/"
--- src/components/sections/Service/Service.js
+++ src/components/sections/Service/Service.js
@@ -13,7 +13,7 @@ const Service = () => {
       <div className="container pt-2 pb-5">
         <div className="section-header pt-5 pb-5 text-center">
           <h3 className="section-title">
-            <span>Çalışma </span>Alanlarımız
+            <span>Verilen </span>Hizmetler
           </h3>
           <h6 className="section-subtitle mr-auto ml-auto"></h6>
         </div>
